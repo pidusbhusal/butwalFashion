d23591fbf52ac9ecc751ce808f66ce01f030e33a
Sudip Bhusal <pidusbhusal@gmail.com>
2024-08-26 16:42:23 -0400

    started woking in product page

--- src/Components/ProductPage/index.jsx
+++ src/Components/ProductPage/index.jsx
@@ -28,7 +28,7 @@ function ProductPage() {
   };
 
   return (
-    <div className="container mx-auto w-full p-4 pt-2">
+    <div className="container max-w-[80%] mt-20 mx-auto w-full p-4 pt-2">
       <div className="flex flex-col md:flex-row gap-4">
         <div className="w-full md:w-1/2">
           <img
@@ -38,14 +38,16 @@ function ProductPage() {
           />
         </div>
         <div className="w-full md:w-1/2">
-          <h1 className="text-3xl font-bold text-gray-900">{product.title}</h1>
-          <p className="text-sm text-gray-500">{product.category}</p>
-          <h2 className="text-2xl font-bold text-gray-700">{product.price}</h2>
-          <div className="flex items-center mb-2">
-            <label
-              htmlFor="quantity"
-              className="block mb-1 text-sm font-medium text-gray-700"
-            >
+          <p className="text-sm text-gray-500 uppercase">{product.category}</p>
+
+          <h1 className="text-xl mt-1 font-semibold text-gray-700">
+            {product.title}
+          </h1>
+          <h2 className="text-2xl mt-2 font-bold text-gray-900">
+            $ {product.price}
+          </h2>
+          <div className=" mt-4 items-center mb-2">
+            <label htmlFor="quantity" className="block mb-1   text-gray-600">
               Quantity
             </label>
             <select
@@ -54,7 +56,7 @@ function ProductPage() {
               onChange={(event) =>
                 setProductCount(parseInt(event.target.value))
               }
-              className="ml-2 pl-2 pr-8 py-1 text-sm text-gray-700 border border-gray-300 rounded-md"
+              className=" text-gray-700 border border-gray-300 rounded py-[1rem] w-10 "
             >
               <option value="1">1</option>
               <option value="2">2</option>
