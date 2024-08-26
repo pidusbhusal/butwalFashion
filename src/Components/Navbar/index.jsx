42d130d8cec5d30e669b30770bc38ba000e95ffd
Sudip Bhusal <pidusbhusal@gmail.com>
2024-08-26 13:23:16 -0400

    cart item count added

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import { useContext } from "react";
+import { CartContext } from "../../Context/cartContext";
 
 function Navbar() {
+  const { cart } = useContext(CartContext);
   return (
     <div className="  w-full py-[1rem]  ">
-      <section className=" flex max-w-[80%] items-center justify-between  m-auto ">
+      <section className=" flex max-w-[75%] items-center justify-between  m-auto ">
         <a href="/" className="logo w-fit">
           <img
             src="icons/logo.svg"
@@ -13,30 +15,18 @@ function Navbar() {
           />
         </a>
         <div className="flex gap-20">
-          <div className="flex gap-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="hover:currentColor"
-              class="size-6"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-              />
-            </svg>
-
-            <a href="/" className="font-semibold hover:text-[#D10B3B]">
-              Home
-            </a>
-          </div>
-
-          <a href="/cart" className="font-semibold hover:text-[#D10B3B]">
-            Cart
+          <a href="/" className="font-semibold hover:text-[#D10B3B]">
+            Home
           </a>
+
+          <div className="flex gap-2 relative">
+            <a href="/cart" className="font-semibold hover:text-[#D10B3B]">
+              Cart
+            </a>
+            <div className="w-5 absolute  bottom-3 left-8 text-white font-bold h-5 rounded-lg bg-red-600">
+              <p className="w-fit m-auto text-sm">{cart.length}</p>
+            </div>
+          </div>
         </div>
       </section>
     </div>
