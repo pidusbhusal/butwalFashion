ba9768307b35868a5b2f234b080aaf88d45c4a4b
Sudip Bhusal <pidusbhusal@gmail.com>
2024-08-27 23:58:09 -0400

    added related product

--- src/Components/ProductPage/index.jsx
+++ src/Components/ProductPage/index.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useContext } from "react";
 import { useParams } from "react-router-dom";
 import { CartContext } from "../../Context/cartContext";
-import relatedProduct from "../RelatedProduct";
+import RelatedProduct from "../RelatedProduct";
 
 function ProductPage() {
   const [productCount, setProductCount] = useState(1);
@@ -29,60 +29,64 @@ function ProductPage() {
   };
 
   return (
-    <div className="container max-w-[80%] mt-16 mx-auto w-full py-lg px-[3rem] drop-shadow-md rounded-md bg-white ">
-      <div className="flex  flex-col md:flex-row gap-8">
-        <div className="w-1/2  image-container">
-          <img
-            src={product.image}
-            alt={product.title}
-            className="w-full h-96  m-auto  object-contain rounded-md "
-          />
-        </div>
-        <div className="w-1/2">
-          <p className="text-sm text-gray-500 uppercase">{product.category}</p>
-
-          <h1 className="text-xl mt-1 font-semibold text-gray-700">
-            {product.title}
-          </h1>
-          <h2 className="text-2xl mt-2 font-bold text-gray-900">
-            $ {product.price}
-          </h2>
-          <div className=" mt-4 items-center mb-2">
-            <label htmlFor="quantity" className="block mb-1   text-gray-600">
-              Quantity
-            </label>
-            <select
-              id="quantity"
-              value={productCount}
-              onChange={(event) =>
-                setProductCount(parseInt(event.target.value))
-              }
-              className=" text-gray-700 border pl-[1rem] border-gray-300 rounded py-[0.7rem] w-20 "
-            >
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-            </select>
+    <div className="container max-w-[80%] mt-8 mx-auto w-full">
+      <div className=" py-sm px-[3rem] drop-shadow-md rounded-md bg-white ">
+        <div className="flex items-center  flex-col md:flex-row gap-8">
+          <div className="w-1/2  image-container">
+            <img
+              src={product.image}
+              alt={product.title}
+              className="w-full h-96  m-auto  object-contain rounded-md "
+            />
           </div>
-          {itemSelected && <p className="text-green-700 mt-4">Item added</p>}
-          <button
-            className="add-to-card mt-1 bg-black text-white py-2 px-4 rounded-md hover:bg-gray-800 transition-all"
-            onClick={handleAddToCart}
-          >
-            Add to cart
-          </button>
+          <div className="w-1/2 border-gray-100 rounded-md px-sm py-sm border-2">
+            <p className="text-sm text-gray-500 uppercase">
+              {product.category}
+            </p>
 
-          <h4 className="text-xm font-semibold mt-4 text-gray-900 mb-2">
-            Description
-          </h4>
-          <p className=" text-gray-600">{product.description}</p>
-          {cart.map((item) => {
-            <p>{item}</p>;
-          })}
+            <h1 className="text-xl mt-1 font-semibold text-gray-700">
+              {product.title}
+            </h1>
+            <h2 className="text-2xl mt-2 font-bold text-gray-900">
+              $ {product.price}
+            </h2>
+            <div className=" mt-4 items-center mb-2">
+              <label htmlFor="quantity" className="block mb-1   text-gray-600">
+                Quantity
+              </label>
+              <select
+                id="quantity"
+                value={productCount}
+                onChange={(event) =>
+                  setProductCount(parseInt(event.target.value))
+                }
+                className=" text-gray-700 border pl-[1rem] border-gray-300 rounded py-[0.7rem] w-20 "
+              >
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+              </select>
+            </div>
+            {itemSelected && <p className="text-green-700 mt-4">Item added</p>}
+            <button
+              className="add-to-card mt-1 bg-black text-white py-2 px-4 rounded-md hover:bg-gray-800 transition-all"
+              onClick={handleAddToCart}
+            >
+              Add to cart
+            </button>
+
+            <h4 className="text-xm font-semibold mt-4 text-gray-900 mb-2">
+              Description
+            </h4>
+            <p className=" text-gray-600">{product.description}</p>
+            {cart.map((item) => {
+              <p>{item}</p>;
+            })}
+          </div>
         </div>
       </div>
-      <relatedProduct />
+      <RelatedProduct category={product.category} productID={product.id} />
     </div>
   );
 }
--- src/Components/RelatedProduct/index.jsx
+++ src/Components/RelatedProduct/index.jsx
@@ -1,5 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import ProductCard from "../ProductCard";
 
-export default function relatedProduct() {
-  return <div>relatedProduct</div>;
+export default function RelatedProduct({ category, productID }) {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products/category/" + category)
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data);
+      })
+      .catch((error) => console.log(error));
+  }, [, productID]);
+  return (
+    <div className="homeSection  bg-white mt-5 py-sm px-sm rounded-md shadow">
+      <h2 className="capitalize text-xl font-semibold mb-4">Related Product</h2>
+      <div className="grid   relative h-fit grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {products ? (
+          products.map((product) => (
+            <Link
+              to={` products/${product.id}`}
+              key={product.id}
+              
+              className="h-full"
+            >
+              <ProductCard
+                id={product.id}
+                title={product.title}
+                price={product.price}
+                category={product.category}
+                image={product.image}
+                rating={product.rating}
+                className="bg-white shadow-md rounded p-4"
+              />
+            </Link>
+          ))
+        ) : (
+          <p>Loading</p>
+        )}
+        {console.log(products)}
+      </div>
+    </div>
+  );
 }
